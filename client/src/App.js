0db535deff246f85c1ef9fe5f529ddaed77128a4
Coder-Srinivas <srinivas.babu364@gmail.com>
2021-10-16 22:56:16 +0530

    Fixed package-lock.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
-
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 export default function App() {
   const { isLoading } = useAuth0();
-if (isLoading) return <div className="loading"><h2><FontAwesomeIcon icon={faSpinner} className="fa-spin"/></h2><h2>Loading...</h2></div>
+  if (isLoading)
+    return (
+      <div className="loading">
+        <h2>
+          <FontAwesomeIcon icon={faSpinner} className="fa-spin" />
+        </h2>
+        <h2>Loading...</h2>
+      </div>
+    );
   return (
     <div className="app">
       <Router>
-        <Navbar/>
+        <Navbar />
         <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/profile" component={Dashboard} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/profile" component={Dashboard} />
         </Switch>
       </Router>
     </div>
-  )
+  );
 }
-
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,17 +1,17 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import JSONPretty from 'react-json-pretty';
+import JSONPretty from "react-json-pretty";
 
 export default function Dashboard() {
-    const { user, isAuthenticated } = useAuth0();
+  const { user, isAuthenticated } = useAuth0();
 
-    return (
-      isAuthenticated && ( 
-       <div>
-          <img src={user.picture} alt={user.name} />
-          <h2>{user.name}</h2>
-          <p>{user.email}</p>
-          <JSONPretty data={user} />
-        </div>
-      )
+  return (
+    isAuthenticated && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <JSONPretty data={user} />
+      </div>
     )
+  );
 }
